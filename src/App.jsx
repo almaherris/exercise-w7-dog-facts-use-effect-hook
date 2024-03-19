3740f2005452dd47a23a1ce072137a040b71f729
Alma Herrström <almaherrstrom@Almas-MacBook-Air-4.local>
2024-03-19 16:15:42 +0100

    completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 // Import required  hooks
-import { DogFact } from "./components/DogFact";
+import { useEffect, useState } from "react"
+import "./styles.css"
+import { DogFact } from "./components/DogFact"
 
 export const App = () => {
   // Hint: Initialize state for storing the dog fact
+  const [dogFact, setDogFact] = useState()
+
   // Hint: Define the API endpoint
+  const URL = "https://dogapi.dog/api/v2/facts"
+
   // Hint: Create a function to fetch the dog fact
+  const fetchDogFact = () => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+        setDogFact(json)
+      })
+  }
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
+  useEffect(() => {
+    fetchDogFact()
+  }, [])
 
   return (
     <div className="App">
-      <DogFact />
+      {dogFact ? (
+        <DogFact dogFact={dogFact} />
+      ) : (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
-  );
-};
+  )
+}
